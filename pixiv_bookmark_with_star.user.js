50b307a438398d9952a79350f508e3e6b3f6e3a5
oga <8monkey.theorem@gmail.com>
2012-09-05 23:06:16 +0900

    follow pixiv's change.

--- pixiv_bookmark_with_star.user.js
+++ pixiv_bookmark_with_star.user.js
@@ -1,44 +1,55 @@
 // ==UserScript==
-// @name	pixiv_bookmark_with_star
-// @description	ブックマークするときに同時に10点評価する Requirement: Chrome or Firefox
-// @version	1.1
+// @name    pixiv_bookmark_with_star
+// @description    ブックマークするときに同時に10点評価する Requirement: Chrome or Firefox
+// @version    1.1
 // @namespace    http://www1.icnet.ne.jp/a7v83w2r/
-// @include	http://www.pixiv.net/member_illust.php?mode=medium&illust_id=*
-// @include	http://www.pixiv.net/novel/show.php?id=*
-// @match	http://www.pixiv.net/member_illust.php?mode=medium&illust_id=*
-// @match	http://www.pixiv.net/novel/show.php?id=*
+// @include    http://www.pixiv.net/member_illust.php?mode=medium&illust_id=*
+// @include    http://www.pixiv.net/novel/show.php?id=*
+// @match    http://www.pixiv.net/member_illust.php?mode=medium&illust_id=*
+// @match    http://www.pixiv.net/novel/show.php?id=*
 // ==/UserScript==
 
 (function(){
 
-var isRated = function() {document.getElementById("unit").getElementsByClassName("current-rating")[0] != undefined};
-//if already rated
+/*Main Logic
+if(!$('.rating').hasClass('rated')) {
+    pixiv.rating.rate = 10;
+    $('.rating').click();
+}
+*/
+
+var isRated = function() {
+    document.querySelector('.rated') != null
+};
+
 if(isRated()) {
-	return;
+    return;
 }
 
-var elem = document.getElementsByClassName("bookmark")[1].getElementsByTagName("a")[0];
-var href = elem.href;
-elem.href = "javascript:void 0";
-var handler = function() {
-	elem.addEventListener("click", function(){
-		var timeout = 50;
-		//if not already rated
-		if(!isRated()) {
-			var countup = "javascript:countup_rating(10);"
-			location.href = countup;
-			var timer = setInterval(function(){
-				if(!isRated()) {
-					clearInterval(timer);
-					location.href = href;
-				}
-			}, timeout);
-		} else {
-			location.href = href;
-		}
 
-	},false);
+var bookmark = document.querySelector('.bookmark a.ui-button');
+var href = bookmark.href;
+bookmark.href = 'javascript:void 0';
+
+var addHandler = function() {
+    bookmark.addEventListener('click', function(){
+        var timeout = 50;
+        
+        if(!isRated()) {
+            var countup = 'javascript:pixiv.rating.rate = 10;$(".rating").click();';
+            location.href = countup;
+            var timer = setInterval(function(){
+                if(!isRated()) {
+                    clearInterval(timer);
+                    location.href = href;
+                }
+            }, timeout);
+        } else {
+            location.href = href;
+        }
+    },false);
 };
-handler();
+
+addHandler();
 
 })();
