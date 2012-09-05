bb8b9134b27d9b8b69ba404cce187fd1f134bd03
oga <8monkey.theorem@gmail.com>
2012-09-06 01:30:38 +0900

    Converted into CoffeeScript. Adapt pixiv novel.

---
+++ pixiv_bookmark_with_star.user.coffee
@@ -0,0 +1,58 @@
+`
+// ==UserScript==
+// @name    pixiv_bookmark_with_star
+// @description    ブックマークするときに同時に10点評価する Requirement: Chrome or Firefox
+// @version    1.2
+// @namespace    http://www1.icnet.ne.jp/a7v83w2r/
+// @include    http://www.pixiv.net/member_illust.php?mode=medium&illust_id=*
+// @include    http://www.pixiv.net/novel/show.php?id=*
+// @match    http://www.pixiv.net/member_illust.php?mode=medium&illust_id=*
+// @match    http://www.pixiv.net/novel/show.php?id=*
+// ==/UserScript==
+`
+
+pixiv_bookmark_with_star = ->
+    isIllust = ->
+        document.URL.match('illust') != null
+
+    isRated = ->
+        if isIllust()
+            document.querySelector('.rated') != null
+        else
+            document.querySelector('#unit .current-rating') != null
+    
+    rate = ->
+        if isIllust()
+            countup = 'javascript:pixiv.rating.rate = 10;$(".rating").click();'
+        else
+            countup = 'javascript:countup_rating(10);'
+        location.href = countup
+
+    if isRated()
+        return
+
+
+    bookmark = document.querySelector('.bookmark a.ui-button')
+
+    if not bookmark?
+        return
+
+    original_href = bookmark.href
+    bookmark.href = 'javascript:void 0'
+
+    bookmark.addEventListener('click', ->
+        timeout = 50
+        
+        if not isRated()
+            rate()
+            timer = setInterval( ->
+                if isRated()
+                    clearInterval(timer)
+                    location.href = original_href
+            , timeout)
+        else
+            location.href = original_href;
+    , false)
+
+
+pixiv_bookmark_with_star()
--- pixiv_bookmark_with_star.user.js
+++ pixiv_bookmark_with_star.user.js
@@ -1,55 +1,58 @@
+
 // ==UserScript==
 // @name    pixiv_bookmark_with_star
 // @description    ブックマークするときに同時に10点評価する Requirement: Chrome or Firefox
-// @version    1.1
+// @version    1.2
 // @namespace    http://www1.icnet.ne.jp/a7v83w2r/
 // @include    http://www.pixiv.net/member_illust.php?mode=medium&illust_id=*
 // @include    http://www.pixiv.net/novel/show.php?id=*
 // @match    http://www.pixiv.net/member_illust.php?mode=medium&illust_id=*
 // @match    http://www.pixiv.net/novel/show.php?id=*
 // ==/UserScript==
+;
+var pixiv_bookmark_with_star;
 
-(function(){
-
-/*Main Logic
-if(!$('.rating').hasClass('rated')) {
-    pixiv.rating.rate = 10;
-    $('.rating').click();
-}
-*/
-
-var isRated = function() {
-    document.querySelector('.rated') != null
-};
-
-if(isRated()) {
-    return;
-}
-
-
-var bookmark = document.querySelector('.bookmark a.ui-button');
-var href = bookmark.href;
-bookmark.href = 'javascript:void 0';
-
-var addHandler = function() {
-    bookmark.addEventListener('click', function(){
-        var timeout = 50;
-        
-        if(!isRated()) {
-            var countup = 'javascript:pixiv.rating.rate = 10;$(".rating").click();';
-            location.href = countup;
-            var timer = setInterval(function(){
-                if(!isRated()) {
-                    clearInterval(timer);
-                    location.href = href;
-                }
-            }, timeout);
-        } else {
-            location.href = href;
+pixiv_bookmark_with_star = function() {
+  var bookmark, isIllust, isRated, original_href, rate;
+  isIllust = function() {
+    return document.URL.match('illust') !== null;
+  };
+  isRated = function() {
+    if (isIllust()) {
+      return document.querySelector('.rated') !== null;
+    } else {
+      return document.querySelector('#unit .current-rating') !== null;
+    }
+  };
+  rate = function() {
+    var countup;
+    if (isIllust()) {
+      countup = 'javascript:pixiv.rating.rate = 10;$(".rating").click();';
+    } else {
+      countup = 'javascript:countup_rating(10);';
+    }
+    return location.href = countup;
+  };
+  if (isRated()) return;
+  bookmark = document.querySelector('.bookmark a.ui-button');
+  if (!(bookmark != null)) return;
+  original_href = bookmark.href;
+  bookmark.href = 'javascript:void 0';
+  return bookmark.addEventListener('click', function() {
+    var timeout, timer;
+    timeout = 50;
+    if (!isRated()) {
+      rate();
+      return timer = setInterval(function() {
+        if (isRated()) {
+          clearInterval(timer);
+          return location.href = original_href;
         }
-    },false);
+      }, timeout);
+    } else {
+      return location.href = original_href;
+    }
+  }, false);
 };
 
-addHandler();
-
-})();
+pixiv_bookmark_with_star();
